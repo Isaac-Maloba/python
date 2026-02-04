94a6ed94263e50839569a537c099c24f0fe5dc7f
Isaac-Maloba <isaaccmaloba@gmail.com>
2026-02-04 23:07:21 +0300

    added assignment

---
+++ assignment1.py
@@ -0,0 +1,12 @@
+weight = 78
+height = 1.75
+exponent = height**2
+bmi = weight/exponent
+print("The BMI of this person is: ", bmi)
+
+length = 48
+width = 25
+perimeter = 2*(length+width)
+area = length*width
+print("The perimeter of the rectangle is: ", perimeter)
+print("The area of the rectangle is: ", area)
---
+++ lesson1d.py
@@ -0,0 +1,15 @@
+# Create a python program that is able to calculate the simple interest given the principle as 50000, rate as 5% and time as 8 years. SI = (P*R*T)/100
+
+principle = 50000
+rate = 5
+time = 8
+
+simple_interest = (principle*rate*time)/100
+
+print("The simple interest is: ", simple_interest)
+
+# Assignment
+# 1. Write a python program that is able to calculate the BMI of a person whose weight is 78kgs and height is 1.75 metres. The formula is weight/(height squared)
+#2. Find the area and perimeter of a rectangle of length 48cm and width is 25cm
+
+# Research on python list, typle and dictionary data types
